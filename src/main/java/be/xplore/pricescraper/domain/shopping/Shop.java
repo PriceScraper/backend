fd984907958843091c9509d91520e35a104cff5d
TimTrademark <truyens.tim@telenet.be>
2023-04-17 11:35:21 +0200

    optimized imports
    architecture setup

---
+++ src/main/java/be/xplore/pricescraper/domain/shopping/Shop.java
@@ -0,0 +1,17 @@
+package be.xplore.pricescraper.domain.shopping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Entity
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String url;
+}
--- src/test/java/be/xplore/pricescraper/PriceScraperApplicationTests.java
+++ src/test/java/be/xplore/pricescraper/PriceScraperApplicationTests.java
@@ -1,4 +1,4 @@
-package be.xplore.price_scraper;
+package be.xplore.pricescraper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
