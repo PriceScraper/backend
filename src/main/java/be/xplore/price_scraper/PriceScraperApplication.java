8936884243613b97574e335e9ce829e5fa515952
TimTrademark <truyens.tim@telenet.be>
2023-04-17 10:26:26 +0200

    initial setup

---
+++ src/main/java/be/xplore/price_scraper/PriceScraperApplication.java
@@ -0,0 +1,13 @@
+package be.xplore.price_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceScraperApplication.class, args);
+    }
+
+}
