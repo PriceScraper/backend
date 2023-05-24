9433b6d4004d28f3d00551824e4c75266b55a92d
Sander Constantin <sander.constantin@student.kdg.be>
2023-05-24 17:10:20 +0200

    feat: Barcode

--- adapters/incoming/rest/src/main/java/be/xplore/pricescraper/controllers/ItemsController.java
+++ adapters/incoming/rest/src/main/java/be/xplore/pricescraper/controllers/ItemsController.java
@@ -2,11 +2,16 @@ package be.xplore.pricescraper.controllers;
 
 import be.xplore.pricescraper.domain.shops.Item;
 import be.xplore.pricescraper.domain.shops.TrackedItem;
+import be.xplore.pricescraper.dtos.BarcodeScanResult;
 import be.xplore.pricescraper.dtos.ItemSearchDto;
 import be.xplore.pricescraper.dtos.TrackItem;
+import be.xplore.pricescraper.exceptions.ItemNotFoundException;
+import be.xplore.pricescraper.services.BarcodeItemService;
 import be.xplore.pricescraper.services.ItemService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 @AllArgsConstructor
+@Slf4j
 public class ItemsController {
   private final ItemService itemService;
+  private final BarcodeItemService barcodeItemService;
 
   @GetMapping("/{id}")
   public Item findItemByIdWithLatestPrices(@PathVariable int id) {
@@ -35,6 +42,22 @@ public class ItemsController {
   @GetMapping
   public List<ItemSearchDto> findItemsByNameLike(@RequestParam String name) {
     return itemService.findItemByNameLike(name);
+  }
+
+  /**
+   * Finds item name from barcode.
+   */
+  @GetMapping("/code")
+  public ResponseEntity<BarcodeScanResult> findItemsByBarcode(@RequestParam String q) {
+    try {
+      var query = barcodeItemService.getProductName(q);
+      return ResponseEntity.ok(new BarcodeScanResult(true, query));
+    } catch (ItemNotFoundException e) {
+      return ResponseEntity.ok(new BarcodeScanResult(false, e.getMessage()));
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return ResponseEntity.badRequest().build();
+    }
   }
 
   /**
---
+++ adapters/incoming/rest/src/main/java/be/xplore/pricescraper/dtos/BarcodeScanResult.java
@@ -0,0 +1,7 @@
+package be.xplore.pricescraper.dtos;
+
+/**
+ * Entity to transfer data.
+ */
+public record BarcodeScanResult(boolean succeeded, String result) {
+}
